226a2168652c5519f98e638e79ced4f0e9dd1620
Vladi <39379713+vladik93@users.noreply.github.com>
2023-11-22 21:35:19 +0200

    refactoring newlist.js

--- js/newlist.js
+++ js/newlist.js
@@ -8,23 +8,84 @@ const icons = ["fish", "cheese", "carrot", "pizza-slice", "cookie"];
 includeHeader();
 
 /**
- * Returns  a random icon.
+ * Generates a random icon.
  */
-const generateRandomIcon = () => {
-  console.log("generateRandomIcon ===>");
+const renderRandomIcon = () => {
   let index = Math.floor(Math.random() * icons.length);
   let icon = `fa-solid fa-${icons[index]}`;
-  console.log(index);
 
-  return `<i class="${icon} icon--mdx2">`;
+  // return `<i class="${icon} icon--mdx2">`;
+
+  const randomIconWrapperEl = document.createElement("div");
+  randomIconWrapperEl.classList.add("random-icon-wrapper");
+
+  randomIconWrapperEl.innerHTML = `<i class="${icon} icon--mdx2"></i>`;
+
+  containerEl.appendChild(randomIconWrapperEl);
 };
 
+/**
+ * Generates input box for adding new lists.
+ */
+
+const renderNewListInput = () => {
+  const newListInputWrapper = document.createElement("div");
+  newListInputWrapper.classList.add("new-list-input-wrapper");
+
+  newListInputWrapper.innerHTML = `<input type="text" class="input input--full" />`;
+
+  containerEl.appendChild(newListInputWrapper);
+};
+
+/**
+ * Renders suggestion buttons in HTML.
+ * @param {*} containerEl - suggestions container element
+ */
+const renderSuggestionElement = (containerEl) => {
+  const suggestionsEl = document.createElement("div");
+  suggestionsEl.classList.add("suggestions");
+
+  suggestions.map((suggestion) => {
+    const suggestionEl = document.createElement("button");
+    suggestionEl.classList.add("suggestion", "button--secondary");
+    suggestionEl.innerText = suggestion;
+
+    suggestionsEl.appendChild(suggestionEl);
+  });
+
+  containerEl.appendChild(suggestionsEl);
+};
+
+const renderSuggestions = () => {
+  const suggestionsWrapperEl = document.createElement("div");
+  suggestionsWrapperEl.classList.add("suggestions-wrapper");
+
+  suggestionsWrapperEl.innerHTML = `<span class="suggestions-title text--sm">Suggestions</span>`;
+
+  renderSuggestionElement(suggestionsWrapperEl);
+
+  containerEl.appendChild(suggestionsWrapperEl);
+};
+
+/**
+ * Renders the button for creating the new list.
+ */
+const renderNewListButton = () => {
+  const newListButtonWrapperEl = document.createElement("div");
+  newListButtonWrapperEl.classList.add("new-list-button-wrapper");
+  newListButtonWrapperEl.innerHTML = `<button class="new-list-button button--full button button--primary">CREATE</button>`;
+
+  containerEl.appendChild(newListButtonWrapperEl);
+};
+
+/**
+ * Renders the full new list page.
+ */
 const renderNewListPage = () => {
-  const newListEl = document.createElement("div");
-  newListEl.classList.add("new-list");
-
-  const newListWrapperEl = document.createElement("div");
-  newListWrapperEl.classList.add("new-list-wrapper");
-
-  containerEl.appendChild(newListEl);
+  renderRandomIcon();
+  renderNewListInput();
+  renderSuggestions();
+  renderNewListButton();
 };
+
+renderNewListPage();
