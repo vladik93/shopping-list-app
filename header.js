4e240ca385f5eeedb60bae17d898ee28fd34ed07
Vladi <39379713+vladik93@users.noreply.github.com>
2023-11-25 17:26:45 +0200

    refactored header.js
    Note: still deciding whether the refactored approach is better.

--- header.js
+++ header.js
@@ -1,10 +1,5 @@
 import { getCurrentTime } from "./functions.js";
 
-const handleBackButtonClick = () => {
-  history.back();
-  sessionStorage.clear();
-};
-
 const setHeaderOptions = (title, isToggler, actions = []) => {
   let headerOptions = {
     title,
@@ -25,8 +20,8 @@ const onHandleListShareClick = () => {
 // HEADER OPTIONS
 
 const mainHeaderOpts = setHeaderOptions("My Lists", true, [
-  { icon: "user-plus", listener: onHandleListShareClick },
-  { icon: "envelope", listener: onHandleListShareClick },
+  { action: "list-share", icon: "user-plus" },
+  { action: "account-open", icon: "envelope" },
 ]);
 
 const newListOpts = setHeaderOptions("", false, []);
@@ -50,48 +45,57 @@ const renderDynamicHeader = ({ title, isToggler, actionsArr }) => {
   headerEl.classList.add("header");
 
   if (isToggler) {
-    const headerSidenavBtn = document.createElement("button");
-    headerSidenavBtn.classList.add("button--icon", "header-sidenav-button");
-    headerEl.appendChild(headerSidenavBtn);
-
-    const headerSidenavIcon = document.createElement("i");
-    headerSidenavIcon.classList.add("fa-solid", "fa-bars", "icon--md");
-    headerSidenavBtn.appendChild(headerSidenavIcon);
+    headerEl.innerHTML += `<button class="button--icon header-sidenav-button">
+      <i class="fa-solid fa-bars icon--md">
+    </button>`;
   } else {
-    const headerBackBtn = document.createElement("button");
-    headerBackBtn.classList.add("button--icon", "header-sidenav-button");
-    headerEl.appendChild(headerBackBtn);
-
-    const headerBackIcon = document.createElement("i");
-    headerBackIcon.classList.add("fa-solid", "fa-arrow-left", "icon--md");
-    headerBackBtn.appendChild(headerBackIcon);
-
-    headerBackBtn.addEventListener("click", handleBackButtonClick);
+    headerEl.innerHTML += `<button class="button--icon header-sidenav-button" data-action="back">
+      <i class="fa-solid fa-arrow-left icon--md"></i>
+    </button>`;
   }
 
-  const headerTitleEl = document.createElement("h3");
-  headerTitleEl.classList.add("header-title");
-  headerTitleEl.innerText = title;
-  headerEl.appendChild(headerTitleEl);
+  headerEl.innerHTML += `<h3 class="header-title">${title}</h3>`;
 
   const headerActionsEl = document.createElement("div");
   headerActionsEl.classList.add("header-actions");
   headerEl.appendChild(headerActionsEl);
 
-  actionsArr.map((action) => {
-    const headerActionBtn = document.createElement("button");
-    headerActionBtn.classList.add("header-action", "button--icon");
+  headerActionsEl.innerHTML += actionsArr
+    .map((action) => {
+      return `<button class="header-action button--icon" id="${action.action}">
+      <i class="fa-solid fa-${action.icon} icon--md"></i>
+    </button>`;
+    })
+    .join("");
 
-    const headerActionIcon = document.createElement("i");
-    headerActionIcon.classList.add("fa-solid", `fa-${action.icon}`, "icon--md");
-
-    headerActionBtn.appendChild(headerActionIcon);
-
-    headerActionsEl.appendChild(headerActionBtn);
-
-    headerActionBtn.addEventListener("click", action.listener);
-  });
   headerEl.appendChild(headerActionsEl);
 
   document.body.appendChild(headerEl);
+
+  // EVENT LISTENERS
+
+  headerActionsEl.addEventListener("click", handleHeaderActions);
+  headerEl.addEventListener("click", handleHeader);
+};
+
+const handleHeaderActions = (e) => {
+  let element = e.target;
+  let elementParent = element.parentElement;
+
+  if (elementParent.id === "list-share") {
+    console.log("list-share triggered ===>");
+  } else if (elementParent.id === "account-open") {
+    console.log("account-open triggered ====>");
+  }
+};
+
+const handleHeader = (e) => {
+  let element = e.target;
+  let parentElement = element.parentElement;
+  let action = parentElement.dataset.action;
+
+  if (action === "back") {
+    history.back();
+    sessionStorage.clear();
+  }
 };
