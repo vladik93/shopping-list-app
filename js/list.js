8ff2123fa6279ff9d1d8fd2d375ad6b44c320800
Vladi <39379713+vladik93@users.noreply.github.com>
2023-11-27 10:01:38 +0200

    update

--- js/list.js
+++ js/list.js
@@ -9,9 +9,22 @@ const searchParams = new URL(url).searchParams;
 const entries = new URLSearchParams(searchParams).values();
 
 const array = Array.from(entries);
-
 let listId = array[0];
 
-let isFound = lists.some((list) => list.id === parseInt(listId));
+// let isFound = lists.some((list) => list.id === parseInt(listId));
 
-console.log("isFound ===>", isFound);
+function renderListPage(elm) {
+  const listPageEl = document.createElement("div");
+  listPageEl.classList.add("list-page");
+
+  if (lists.find((item) => item.id === elm.id)) {
+    let found = item;
+    let listItemEl = document.createElement("div");
+    listItemEl.classList.add("list-item");
+  } else {
+  }
+
+  listPageEl.appendChild();
+}
+
+renderListPage(listId);
