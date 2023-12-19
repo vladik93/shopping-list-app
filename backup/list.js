7fa9824ea301239772ce98c9b7c9ce2e93de2989
Vladi <39379713+vladik93@users.noreply.github.com>
2023-12-19 23:45:12 +0200

    redoing the list-item-check component

--- backup/list.js
+++ backup/list.js
@@ -1,10 +1,7 @@
-import { lists, icons } from "../data.js";
-import { navigateToPage } from "../functions.js";
-import { includeHeader } from "../header.js";
+console.log("~ list.js ~");
 
-console.log("list.js ====>");
-
-includeHeader();
+import { lists, items, icons } from "../data.js";
+import { navigateToPageWithId } from "../functions.js";
 
 const containerEl = document.querySelector(".container");
 
@@ -13,65 +10,174 @@ const searchParams = new URL(url).searchParams;
 const entries = new URLSearchParams(searchParams).values();
 const array = Array.from(entries);
 
-const randomIconIndex = sessionStorage.getItem("randomIconIndex") || 0;
+const listId = array[0];
 
-let listId = array[0];
+let listById = [];
 
-let newItemUrl = "http://localhost:5500/newitem.html?";
+console.log("listById ===>", listById);
 
-console.log(lists);
+const getRandomIconIndex = () => {
+  console.log("getRandomIconIndex >>>");
+  let index = undefined;
+  const storage = sessionStorage.getItem("randomIconIndex");
+  if (storage !== null) {
+    console.log("storage found!");
+    index = storage;
+  } else {
+    console.log("storage not found!");
+    index = Math.floor(Math.random() * icons.length);
+    sessionStorage.setItem("randomIconIndex", index);
+  }
 
-function renderListPage(listId) {
-  console.log("renderListPage ===>");
-  console.log("elm param ====>", listId);
+  return index;
+};
 
-  console.log("listItems ===>", lists);
+const randomIconIndex = getRandomIconIndex();
 
-  if (lists.some((item) => item.id == listId)) {
-    const found = lists.find((item) => item.id === parseInt(listId));
+const getItemsByListId = () => {
+  console.log("getItemsByListId ===>");
+  const filteredList = items.filter((item) => item.listId === parseInt(listId));
 
-    const listPageEl = document.createElement("div");
-    listPageEl.classList.add("list-page");
+  listById = filteredList;
+};
 
-    listPageEl.innerHTML = `
+const renderListPage = (listPageId) => {
+  console.log("renderListPage >>>");
+
+  containerEl.innerHTML = "";
+
+  const listPageWrapperEl = document.createElement("div");
+  listPageWrapperEl.classList.add("list-page-wrapper");
+
+  const progressWrapperEl = document.createElement("div");
+  progressWrapperEl.classList.add("progress-wrapper");
+  progressWrapperEl.innerHTML = `
+      <div class="progress">
+        <span class="progress-bar"></span>
+      </div>`;
+
+  listPageWrapperEl.insertAdjacentElement("afterbegin", progressWrapperEl);
+
+  if (!listById.length) {
+    listPageWrapperEl.classList.add("empty-list");
+
+    const mainWrapperEl = document.createElement("div");
+    mainWrapperEl.classList.add("main-wrapper");
+    mainWrapperEl.innerHTML = `
       <i class="fa-solid fa-${icons[randomIconIndex]} icon--mdx3 list-icon"></i>
       <p class="primary-text main-title">Let's plan your shopping</p>
-        <p class="secondary-text">
-          Tap the plus button to create your first list
-        </p>
-        
-        <div class="list-button-wrapper">
-          <button class="add-item-button button button--lg button--primary" id="add-item">
-            <i class="fa-solid fa-plus"></i>
-            <span>ADD</span>
-          </button>
-        </div>
+      <p class="secondary-text">Tap the plus button to create your first list</p>
     `;
+    listPageWrapperEl.appendChild(mainWrapperEl);
+  } else {
+    const itemsWrapperEl = document.createElement("div");
+    itemsWrapperEl.classList.add("items-wrapper");
+    listById.map((listItem) => {
+      itemsWrapperEl.innerHTML += renderListItem(listItem);
+    });
 
-    containerEl.appendChild(listPageEl);
+    listPageWrapperEl.appendChild(itemsWrapperEl);
 
-    listPageEl.addEventListener("click", handleListPageClick);
+    itemsWrapperEl.addEventListener("click", onItemsClick);
   }
-}
+
+  const listButtonWrapperEl = document.createElement("div");
+  listButtonWrapperEl.classList.add("list-button-wrapper");
+
+  listButtonWrapperEl.innerHTML = `
+    <button class="add-item-button button button--lg button--primary" id="add-item" data-button-action="add-item">
+      <i class="fa-solid fa-plus"></i>
+      <span>ADD</span>
+    </button>`;
+
+  listPageWrapperEl.insertAdjacentElement("beforeend", listButtonWrapperEl);
+
+  containerEl.appendChild(listPageWrapperEl);
+
+  listPageWrapperEl.addEventListener("click", handleListPageClick);
+};
+
+const renderListItem = (item) => {
+  console.log("renderListItem >>>");
+
+  const { id, title, isDone } = item;
+  console.log("item ===>", item);
+
+  console.log("isDone ===>", isDone);
+  const listItemTemplate = `
+  <div class="item ${isDone ? "checked" : ""}" id=${id}>
+      <label for="item-checkbox" class="checkbox-wrapper">
+        <input type="checkbox" id="item-checkbox-${id}" class="checkbox item-checkbox" checked=${
+    isDone ? true : false
+  } />
+        <i class="fa-solid fa-check item-check-icon"></i>
+      </label>
+      <h4 class="item-content">${title}</h4>
+      <div class="icon-wrapper">
+        <i class="fa-solid fa-cheese icon"></i>
+      </div>
+    
+  </div>`;
+
+  return listItemTemplate;
+};
 
 const handleListPageClick = (e) => {
-  console.log("handleListPageClick ===>");
+  console.log("handleListPageClick >>>");
+
   let target = e.target;
 
-  if (target.closest("#add-item")) {
-    navigateToNewTasksPage(listId);
+  if (target.closest("[data-button-action='add-item']")) {
+    navigateToPageWithId("newitem", listId);
   }
 };
 
-const navigateToNewTasksPage = (listId) => {
-  const searchParams = new URLSearchParams({ id: listId });
-  const queryString = searchParams.toString();
+const onItemsClick = (e) => {
+  console.log("onItemsClick ===>");
+  let target = e.target;
 
-  window.location.href = newItemUrl + queryString;
+  if (target.closest(".item-checkbox")) {
+    let selected = target.closest(".item-checkbox");
+
+    let item = selected.closest(".item");
+    item.classList.toggle("checked");
+    !!selected.checked;
+
+    setItemIsDone(item.id);
+  }
 };
 
-renderListPage(listId);
+const setItemIsDone = (itemId) => {
+  console.log("setItemIsDone >>>");
+  let newArray = items.map((item) => {
+    if (item.id === parseInt(itemId)) {
+      return { ...item, isDone: !item.isDone };
+    } else {
+      return item;
+    }
+  });
 
-// window.addEventListener("beforeunload", () => {
-//   localStorage.removeItem("CURRENT_LIST");
-// });
+  localStorage.setItem("ITEMS", JSON.stringify(newArray));
+  getItemsByListId();
+  console.log("listById ===>", listById);
+};
+
+const renderList = () => {
+  console.log("renderList >>>");
+  let itemsWrapperEl = document.querySelector(".items-wrapper");
+
+  itemsWrapperEl.innerHTML = "";
+
+  console.log("listById ===>", listById);
+  listById.map((listItem) => {
+    itemsWrapperEl.innerHTML += renderListItem(listItem);
+  });
+
+  // listById.map((listItem) => {
+  //   itemsWrapperEl.innerHTML += renderListItem(listItem);
+  // });
+};
+
+getItemsByListId();
+
+renderListPage(listId);
--- js/list.js
+++ js/list.js
@@ -4,6 +4,7 @@ import { lists, items, icons } from "../data.js";
 import { navigateToPageWithId } from "../functions.js";
 
 const containerEl = document.querySelector(".container");
+const itemsWrapperEl = document.querySelector(".items-wrapper");
 
 const url = window.location.href;
 const searchParams = new URL(url).searchParams;
@@ -16,24 +17,6 @@ let listById = [];
 
 console.log("listById ===>", listById);
 
-const getRandomIconIndex = () => {
-  console.log("getRandomIconIndex >>>");
-  let index = undefined;
-  const storage = sessionStorage.getItem("randomIconIndex");
-  if (storage !== null) {
-    console.log("storage found!");
-    index = storage;
-  } else {
-    console.log("storage not found!");
-    index = Math.floor(Math.random() * icons.length);
-    sessionStorage.setItem("randomIconIndex", index);
-  }
-
-  return index;
-};
-
-const randomIconIndex = getRandomIconIndex();
-
 const getItemsByListId = () => {
   console.log("getItemsByListId ===>");
   const filteredList = items.filter((item) => item.listId === parseInt(listId));
@@ -41,95 +24,33 @@ const getItemsByListId = () => {
   listById = filteredList;
 };
 
-const renderListPage = (listPageId) => {
-  console.log("renderListPage >>>");
+const renderList = () => {
+  itemsWrapperEl.innerHTML = "";
 
-  containerEl.innerHTML = "";
+  itemsWrapperEl.classList.add("items-wrapper");
+  itemsWrapperEl.addEventListener("click", onItemsClick);
 
-  const listPageWrapperEl = document.createElement("div");
-  listPageWrapperEl.classList.add("list-page-wrapper");
+  if (listById.length) {
+    console.log("listById -> length", listById.length);
 
-  const progressWrapperEl = document.createElement("div");
-  progressWrapperEl.classList.add("progress-wrapper");
-  progressWrapperEl.innerHTML = `
-      <div class="progress">
-        <span class="progress-bar"></span>
-      </div>`;
-
-  listPageWrapperEl.insertAdjacentElement("afterbegin", progressWrapperEl);
-
-  if (!listById.length) {
-    listPageWrapperEl.classList.add("empty-list");
-
-    const mainWrapperEl = document.createElement("div");
-    mainWrapperEl.classList.add("main-wrapper");
-    mainWrapperEl.innerHTML = `
-      <i class="fa-solid fa-${icons[randomIconIndex]} icon--mdx3 list-icon"></i>
-      <p class="primary-text main-title">Let's plan your shopping</p>
-      <p class="secondary-text">Tap the plus button to create your first list</p>
-    `;
-    listPageWrapperEl.appendChild(mainWrapperEl);
-  } else {
-    const itemsWrapperEl = document.createElement("div");
-    itemsWrapperEl.classList.add("items-wrapper");
-    listById.map((listItem) => {
-      itemsWrapperEl.innerHTML += renderListItem(listItem);
+    listById.map(({ id, isDone, title }) => {
+      itemsWrapperEl.innerHTML += `
+        <div class="item ${isDone ? "checked" : ""}" id=${id}>
+          <label for="item-checkbox" class="checkbox-wrapper">
+            <input type="checkbox" id="item-checkbox-${id}" class="checkbox item-checkbox" checked=${
+        isDone ? true : false
+      } />
+            <i class="fa-solid fa-check item-check-icon"></i>
+          </label>
+          <h4 class="item-content">${title}</h4>
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-cheese icon"></i>
+          </div>  
+        </div>`;
     });
-
-    listPageWrapperEl.appendChild(itemsWrapperEl);
-
-    itemsWrapperEl.addEventListener("click", onItemsClick);
   }
 
-  const listButtonWrapperEl = document.createElement("div");
-  listButtonWrapperEl.classList.add("list-button-wrapper");
-
-  listButtonWrapperEl.innerHTML = `
-    <button class="add-item-button button button--lg button--primary" id="add-item" data-button-action="add-item">
-      <i class="fa-solid fa-plus"></i>
-      <span>ADD</span>
-    </button>`;
-
-  listPageWrapperEl.insertAdjacentElement("beforeend", listButtonWrapperEl);
-
-  containerEl.appendChild(listPageWrapperEl);
-
-  listPageWrapperEl.addEventListener("click", handleListPageClick);
-};
-
-const renderListItem = (item) => {
-  console.log("renderListItem >>>");
-
-  const { id, title, isDone } = item;
-  console.log("item ===>", item);
-
-  console.log("isDone ===>", isDone);
-  const listItemTemplate = `
-  <div class="item ${isDone ? "checked" : ""}" id=${id}>
-      <label for="item-checkbox" class="checkbox-wrapper">
-        <input type="checkbox" id="item-checkbox-${id}" class="checkbox item-checkbox" checked=${
-    isDone ? true : false
-  } />
-        <i class="fa-solid fa-check item-check-icon"></i>
-      </label>
-      <h4 class="item-content">${title}</h4>
-      <div class="icon-wrapper">
-        <i class="fa-solid fa-cheese icon"></i>
-      </div>
-    
-  </div>`;
-
-  return listItemTemplate;
-};
-
-const handleListPageClick = (e) => {
-  console.log("handleListPageClick >>>");
-
-  let target = e.target;
-
-  if (target.closest("[data-button-action='add-item']")) {
-    navigateToPageWithId("newitem", listId);
-  }
+  containerEl.appendChild(itemsWrapperEl);
 };
 
 const onItemsClick = (e) => {
@@ -142,42 +63,28 @@ const onItemsClick = (e) => {
     let item = selected.closest(".item");
     item.classList.toggle("checked");
     !!selected.checked;
-
-    setItemIsDone(item.id);
+    setItemIsDone(selected.id);
   }
 };
 
 const setItemIsDone = (itemId) => {
   console.log("setItemIsDone >>>");
+
+  console.log("itemId ===>", itemId);
+  const id = itemId.split("-")[2];
+
   let newArray = items.map((item) => {
-    if (item.id === parseInt(itemId)) {
+    if (item.id === parseInt(id)) {
       return { ...item, isDone: !item.isDone };
     } else {
       return item;
     }
   });
-
   localStorage.setItem("ITEMS", JSON.stringify(newArray));
   getItemsByListId();
-  console.log("listById ===>", listById);
-};
-
-const renderList = () => {
-  console.log("renderList >>>");
-  let itemsWrapperEl = document.querySelector(".items-wrapper");
-
-  itemsWrapperEl.innerHTML = "";
-
-  console.log("listById ===>", listById);
-  listById.map((listItem) => {
-    itemsWrapperEl.innerHTML += renderListItem(listItem);
-  });
-
-  // listById.map((listItem) => {
-  //   itemsWrapperEl.innerHTML += renderListItem(listItem);
-  // });
+  renderList();
 };
 
 getItemsByListId();
 
-renderListPage(listId);
+renderList();
