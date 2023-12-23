4e1b9675c9c5e1f80b54de206e3bb8e6477de0a6
Vladi <39379713+vladik93@users.noreply.github.com>
2023-12-23 09:30:36 +0200

    list/list-item storage updates properly

--- js/list.js
+++ js/list.js
@@ -12,7 +12,7 @@ const array = Array.from(entries);
 
 const listId = parseInt(array[0]);
 
-let listItems = JSON.parse(localStorage.getItem("LIST_ITEMS"));
+let listItems = JSON.parse(localStorage.getItem("LIST_ITEMS")) || [];
 
 const getListItems = () => {
   console.log("getListItems ===>");
@@ -21,6 +21,20 @@ const getListItems = () => {
 };
 
 getListItems();
+
+const updateStorageItems = (itemId) => {
+  console.log("updateItems >>>");
+  if (items.some((item) => item.id == itemId)) {
+    let newItems = items.map((item) => {
+      if (item.id == itemId) {
+        return { ...item, isDone: !item.isDone };
+      } else {
+        return item;
+      }
+    });
+    localStorage.setItem("ITEMS", JSON.stringify(newItems));
+  }
+};
 
 const updateItemIsDone = (itemId) => {
   console.log("updateItemIsDone ===>");
@@ -35,11 +49,10 @@ const updateItemIsDone = (itemId) => {
     });
 
     localStorage.setItem("LIST_ITEMS", JSON.stringify(newItemList));
+
+    updateStorageItems(itemId);
   }
 };
 
-updateItemIsDone(1703286472897);
-updateItemIsDone(1703286472897);
-updateItemIsDone(1703286472897);
 updateItemIsDone(1703286472897);
 updateItemIsDone(1703286472897);
