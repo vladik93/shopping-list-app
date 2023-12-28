7ec7f64e214fe4400a4ba3cb9b8b677ef1f3d8c4
Vladi <39379713+vladik93@users.noreply.github.com>
2023-12-28 23:50:22 +0200

    Update list.js

--- js/list.js
+++ js/list.js
@@ -16,6 +16,7 @@ const listId = parseInt(array[0]);
 let currentList = JSON.parse(localStorage.getItem("CURRENT_LIST")) || {};
 
 let listItems = currentList.listItems;
+console.log("listItems ->", listItems);
 
 const getListById = (listId) => {
   if (lists && lists.length) {
@@ -26,3 +27,51 @@ const getListById = (listId) => {
 };
 
 getListById(listId);
+
+const renderListItems = () => {
+  itemsWrapperEl.innerHTML = "";
+  if (listItems.length) {
+    listItems.map((item) => {
+      const { id, title, isDone } = item;
+      let itemEl = document.createElement("div");
+      itemEl.classList.add("item");
+      itemEl.setAttribute("id", id);
+
+      itemEl.innerHTML = `
+        <label for="item-checkbox" class="checkbox-wrapper">
+          <input type="checkbox" class="checkbox item-checkbox"  />
+          <i class="fa-solid fa-check item-check-icon"></i>
+        </label>
+        <h4 class="item-content">${title}</h4>
+        <div class="icon-wrapper">
+          <i class="fa-solid fa-cheese icon"></i>
+        </div>`;
+
+      itemEl.addEventListener("click", (e) => {
+        let target = e.target;
+        let itemEl = target.closest(".item");
+        let checkboxEl = target.closest(".item-checkbox");
+        let userId = parseInt(itemEl.id);
+
+        if (checkboxEl) {
+        }
+      });
+      itemEl.querySelector(".item-checkbox").checked = isDone ? true : false;
+
+      itemsWrapperEl.appendChild(itemEl);
+    });
+  } else {
+    itemsWrapperEl.classList.add("items-wrapper-empty");
+  }
+};
+
+const updateItemIsDone = (itemId) => {
+  console.log("updateItemIsDone function ->");
+  let list = currentList;
+
+  console.log("list ->", list);
+};
+
+updateItemIsDone();
+
+renderListItems();
